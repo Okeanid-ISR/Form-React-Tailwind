6fb735afc7053aff54fe530765bf07c84f90aeea
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-26 12:04:47 +0200

    Feat: inputs saved in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Header from './components/Header'
-import {useState} from "react";
-import {useEffect} from "react";
+import { useState, useEffect } from "react";
 import Footer from "./components/Footer";
 import Container from "./components/Container";
 import Task from "./components/Task";
+import { v4 as uuidv4 } from 'uuid';
 
 export function App() {
     const [taskList, setTaskList] = useState([]);
     const [completedTaskList, setCompletedTaskList] = useState([]);
-    const [error, setError] = useState("")
-    const [isChecked, setIsChecked] = useState();
+    const [error, setError] = useState("");
+    const [isCheckedMap, setIsCheckedMap] = useState({});
+
+    const uniqueId = uuidv4();
 
     const removeTask = (task) => {
         const newTaskList = taskList.filter((t) => t !== task);
@@ -21,62 +23,72 @@ export function App() {
 
     const removeCompletedTask = (task) => {
         setCompletedTaskList(completedTaskList.filter((t) => t !== task));
-
     }
 
     const handleRemoveTask = (task) => {
         removeTask(task);
         removeCompletedTask(task);
-    }
-
-    const addTask = (task) => {
-        const newTaskList = [...taskList, task];
-
+        const savedTaskList = JSON.parse(localStorage.getItem('taskList')) || [];
+        const newTaskList = savedTaskList.filter((t) => t !== task);
         window.localStorage.setItem("taskList", JSON.stringify(newTaskList));
-
-        setTaskList(newTaskList);
     };
 
-    const handleCompleteTask = (event, task) => {
-        let taskId = task.replace(/\s+/g, '-').toLowerCase();
-        setIsChecked(event.target.checked);
-        if (completedTaskList.includes(task)) {
-            setCompletedTaskList(completedTaskList.filter((t) => t !== task));
-            localStorage.removeItem(taskId);
-        } else {
-            setCompletedTaskList([...completedTaskList, task]);
-            localStorage.setItem(taskId, 'completed');
-        }
+    const addTask = (task) => {
+        if (!task) return;
+        const newTaskList = [...taskList, task];
+        window.localStorage.setItem("taskList", JSON.stringify(newTaskList));
+        setTaskList(newTaskList);
     };
 
     const onSubmit = (task) => {
         setError("");
-
         if (!task) return;
-        if (taskList.includes(task)) {
-          alert("This task is already exists!");
+        if (!taskList) {
+            addTask(task);
             return;
         }
-        addTask(task)
+        if (taskList.includes(task)) {
+            alert("This task already exists!");
+            return;
+        }
+        addTask(task);
+    };
+
+
+    const handleCompleteTask = (event, task) => {
+        const checked = event.target.checked;
+        setIsCheckedMap({
+            ...isCheckedMap,
+            [task]: checked,
+        });
+        if (checked) {
+            setCompletedTaskList([...completedTaskList, task]);
+        } else {
+            setCompletedTaskList(completedTaskList.filter((t) => t !== task));
+        }
     };
 
     useEffect(() => {
         const taskListStr = window.localStorage.getItem("taskList");
-        const taskList = JSON.parse(taskListStr);
-
+        const taskList = JSON.parse(taskListStr) || [];
         setTaskList(taskList);
+
+        const savedCheckedMap = JSON.parse(localStorage.getItem('isCheckedMap')) || {};
+        setIsCheckedMap(savedCheckedMap);
+
+        const completedTaskList = taskList.filter(task => savedCheckedMap[task]);
+        setCompletedTaskList(completedTaskList);
     }, []);
 
     useEffect(() => {
-        localStorage.setItem('isChecked', isChecked);
-    }, [isChecked]);
+        const newCheckedMap = completedTaskList.reduce((acc, task) => {
+            acc[task] = true;
+            return acc;
+        }, {});
+        setIsCheckedMap(newCheckedMap);
 
-
-    useEffect(() => {
-        const initialCheckedState = localStorage.getItem('isChecked') === 'true';
-        setIsChecked(initialCheckedState);
-    }, []);
-
+        window.localStorage.setItem('isCheckedMap', JSON.stringify(newCheckedMap));
+    }, [completedTaskList]);
 
 
     return (
@@ -89,13 +101,15 @@ export function App() {
                 <Task
                     key={index}
                     indx={index + 1}
-                    isChecked={isChecked}
+                    uniqueId={uniqueId}
                     completed={completedTaskList.includes(task)}
+                    isChecked={isCheckedMap[task]}
                     onChange={handleCompleteTask}
                     onRemove={handleRemoveTask}
                 >
                     {task}
                 </Task>
+
             ))}
             <Footer arr={taskList} countChecked={0} completedArr={completedTaskList}/>
         </Container>
