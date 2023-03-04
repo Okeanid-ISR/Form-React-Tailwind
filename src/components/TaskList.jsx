4d014a8f09efffc5a647ac1f4cf4ee1481dde578
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-03-04 23:10:02 +0200

    Refactor: code refactoring + reducer

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
-
-import { useReducer } from "react";
-import { Header } from "./Header";
-import { Task } from "./Task";
-import { taskReducer, errorReducer } from "./reducer";
-import Container from "./Container";
-import Footer from "./Footer";
+import React, {useReducer} from 'react';
+import {Header} from './Header';
+import {Task} from './Task';
+import {initialTaskList} from './state';
+import {taskReducer, errorReducer} from './reducer';
+import Container from './Container';
+import Footer from './Footer';
 import {v4 as uuidv4} from 'uuid';
 
-const uniqueId = uuidv4
+const uniqueId = uuidv4();
+
 export default function TaskList() {
-    // const [taskList, setTaskList] = useLocalStorage("taskList", []);
-    const [taskList, dispatchTaskList] = useReducer(taskReducer, []);
+    const storageKey = "taskList";
+    const [taskList, dispatchTaskList] = useReducer(
+        taskReducer,
+        initialTaskList(storageKey, [])
+    );
     const [error, dispatchError] = useReducer(errorReducer, "");
 
-    const countCompletedTasks = taskList.reduce(
+    const countCompletedTasks = taskList ? taskList.reduce(
         (acc, value) => (value.completed ? acc + 1 : acc),
         0
-    );
+    ) : 0;
+
 
     const handleCleanError = () => {
         dispatchError({ type: "CLEAN_ERROR" });
     };
 
     const handleSubmit = (task) => {
-        // сброс ошибки
         dispatchError({ type: "CLEAN_ERROR" });
-
-        // пустая задача
         if (!task) {
             dispatchError({
                 type: "SHOW_ERROR",
@@ -36,36 +37,48 @@ export default function TaskList() {
             return;
         }
 
-        dispatchTaskList({ type: "ADD_TASK", task });
+        dispatchTaskList({ type: "ADD_TASK", task, storageKey });
     };
 
     const handleCompleteTask = (value, id) => {
-        dispatchTaskList({ type: "COMPLETE_TASK", value, id });
+        dispatchTaskList({
+            type: "COMPLETE_TASK",
+            value,
+            id,
+            storageKey,
+        });
     };
 
     const handleRemoveTask = (id) => {
-        dispatchTaskList({ type: "REMOVE_TASK", id });
+        dispatchTaskList({ type: "REMOVE_TASK", id, storageKey });
     };
 
     return (
         <Container>
             <Header onSubmit={handleSubmit}/>
-            {error && <div>{error}
-                <button onClick={handleCleanError}>x</button>
-            </div>}
+            {error && (
+                <div>
+                    {error}
+                    <button onClick={handleCleanError}>x</button>
+                </div>
+            )}
             {taskList.map((task, index) => (
                 <Task
                     title={task.name}
                     key={task.id}
                     indx={index + 1}
-                    uniqueId={uniqueId}
                     completed={task.completed}
+                    uniqueId={uniqueId}
                     onChange={(value) => handleCompleteTask(value, task.id)}
                     onRemove={() => handleRemoveTask(task.id)}
-                >
-                </Task>
+                />
             ))}
-            <Footer arr={taskList} countChecked={0} completedArr={taskList}/>
+            <Footer
+                arr={taskList}
+                countChecked={countCompletedTasks}
+            />
         </Container>
     );
 }
+
+export default TaskList
